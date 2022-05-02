ae5fbf4dd6771c6b9a15f7ae0a42b6e89e6acde7
JoeyGarber <joseph.z.garber@gmail.com>
2022-05-02 15:24:06 -0400

    Changed route for home button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ const authenticatedOptions = (
     <NavLink to='/change-password' className='nav-link'>Change Password</NavLink>
     <NavLink to='/sign-out' className='nav-link'>Sign Out</NavLink>
     <NavLink to='/create-task' className='nav-link'>Create Task</NavLink>
-    <NavLink to='/' className='nav-link'>Home</NavLink>
+    <NavLink to='/tasks/' className='nav-link'>Home</NavLink>
   </Fragment>
 )
 
